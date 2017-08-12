be5a3b8f1bd84a9c95d11f33aa0949c6c343dda0
Nandbalakrish <30648816+Nandbalakrish@users.noreply.github.com>
2017-08-12 22:06:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
-console.log('Loaded!');
 
-background(255,0,0);
+draw=function()
+{
+    var x= 10;
+    
+    background(255,0,0);
 
 fill(0,0,255);
 rect(120,130,50,50);
 rect(100,75,30,30);
+
+fill(0,255,0);
+wheel(100,200,30,30);
+wheel(100,350,30,30);
+
+x=+10
+
+}
